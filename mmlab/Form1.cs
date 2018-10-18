955afbbc336fd3049f7bb2a4bce4d099649c637f
YazanKhatib <yazankhatib97@gmail.com>
2018-10-18 16:51:30 +0300

    Extracting gray scale using HSL

--- mmlab/Form1.cs
+++ mmlab/Form1.cs
@@ -150,20 +150,37 @@ namespace mmlab
 
         private void grayImageToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (current == null)
-                return;
+
             for (int i = 0; i < current.Width; i++)
             {
                 for (int j = 0; j < current.Height; j++)
                 {
                     Color c = current.GetPixel(i, j);
-                    double value = (c.R + c.G + c.B) / 3; 
-                    Color newC = Color.FromArgb((int)value, (int)value, (int)value);
-                    current.SetPixel(i, j, newC);
+
+                    List<double> values = FromRGB(c.R, c.G, c.B);
+                    values[1] = 0; 
+                    Color cc = ToRGB(values[0], values[1], values[2]);
+                    current.SetPixel(i, j, cc); 
+
                 }
             }
-            picBoxMain.Image = current;
-        }
+            picBoxMain.Image = current; 
+                    /*
+                    if (current == null)
+                        return;
+                    for (int i = 0; i < current.Width; i++)
+                    {
+                        for (int j = 0; j < current.Height; j++)
+                        {
+                            Color c = current.GetPixel(i, j);
+                            double value = (c.R + c.G + c.B) / 3; 
+                            Color newC = Color.FromArgb((int)value, (int)value, (int)value);
+                            current.SetPixel(i, j, newC);
+                        }
+                    }
+                    picBoxMain.Image = current;
+                    */
+                }
 
         private void addColorToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -256,5 +273,104 @@ namespace mmlab
         {
             return new Bitmap(input, w, h);
         }
+
+        public static List<double> FromRGB(Byte R, Byte G, Byte B)
+        {
+            float _R = (R / 255f);
+            float _G = (G / 255f);
+            float _B = (B / 255f);
+
+            float _Min = Math.Min(Math.Min(_R, _G), _B);
+            float _Max = Math.Max(Math.Max(_R, _G), _B);
+            float _Delta = _Max - _Min;
+
+            float H = 0;
+            float S = 0;
+            float L = (float)((_Max + _Min) / 2.0f);
+
+            if (_Delta != 0)
+            {
+                if (L < 0.5f)
+                {
+                    S = (float)(_Delta / (_Max + _Min));
+                }
+                else
+                {
+                    S = (float)(_Delta / (2.0f - _Max - _Min));
+                }
+
+
+                if (_R == _Max)
+                {
+                    H = (_G - _B) / _Delta;
+                }
+                else if (_G == _Max)
+                {
+                    H = 2f + (_B - _R) / _Delta;
+                }
+                else if (_B == _Max)
+                {
+                    H = 4f + (_R - _G) / _Delta;
+                }
+            }
+
+            List<double> l = new List<double>
+            {
+                H,
+                S,
+                L
+            };
+            return l; 
+        }
+
+        public Color ToRGB(double Hue, double Saturation, double Luminosity)
+        {
+            byte r, g, b;
+            if (Saturation == 0)
+            {
+                r = (byte)Math.Round(Luminosity * 255d);
+                g = (byte)Math.Round(Luminosity * 255d);
+                b = (byte)Math.Round(Luminosity * 255d);
+            }
+            else
+            {
+                double t1, t2;
+                double th = Hue / 6.0d;
+
+                if (Luminosity < 0.5d)
+                {
+                    t2 = Luminosity * (1d + Saturation);
+                }
+                else
+                {
+                    t2 = (Luminosity + Saturation) - (Luminosity * Saturation);
+                }
+                t1 = 2d * Luminosity - t2;
+
+                double tr, tg, tb;
+                tr = th + (1.0d / 3.0d);
+                tg = th;
+                tb = th - (1.0d / 3.0d);
+
+                tr = ColorCalc(tr, t1, t2);
+                tg = ColorCalc(tg, t1, t2);
+                tb = ColorCalc(tb, t1, t2);
+                r = (byte)Math.Round(tr * 255d);
+                g = (byte)Math.Round(tg * 255d);
+                b = (byte)Math.Round(tb * 255d);
+            }
+            return Color.FromArgb(r, g, b);
+        }
+        private static double ColorCalc(double c, double t1, double t2)
+        {
+
+            if (c < 0) c += 1d;
+            if (c > 1) c -= 1d;
+            if (6.0d * c < 1.0d) return t1 + (t2 - t1) * 6.0d * c;
+            if (2.0d * c < 1.0d) return t2;
+            if (3.0d * c < 2.0d) return t1 + (t2 - t1) * (2.0d / 3.0d - c) * 6.0d;
+            return t1;
+        }
+
     }
 }
